09b507281be4c64e0a8e1008ac92d53bda554ffb
Alexandru Constantinescu <alexisb1996@gmail.com>
2024-01-18 19:04:39 +0200

    Terminat header

--- header/script.js
+++ header/script.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function () {
+  $('.mobile-dropdown').show();
   $('.hamburger').click(function () {
-    $('header').toggleClass('open');
+    if ($('header').hasClass('open')) {
+      $('.mobile-dropdown').slideUp(function () {
+        $('header').removeClass('open');
+      });
+    } else {
+      $('header').addClass('open');
+      $('.mobile-dropdown').slideDown();
+    }
+  });
+
+  $('.mobile-dropdown li').click(function () {
+    if ($(this).hasClass('selected')) {
+      $('.arrow').removeClass('rotated');
+      $('.submenu-dropdown').slideUp(() => {
+        $(this).removeClass('selected');
+      });
+    } else {
+      $(this).addClass('selected');
+      $('.arrow').addClass('rotated');
+      $('.submenu-dropdown').slideDown();
+    }
   });
 });
